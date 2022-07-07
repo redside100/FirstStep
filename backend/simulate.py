7cc09f40fd02f765e20420e4709a2bc13a824bbb
redside100 <andrewpeng00@gmail.com>
2022-07-07 11:05:00 -0400

    Refactor user generation

--- backend/simulate.py
+++ backend/simulate.py
@@ -1,8 +1,5 @@
-from util import create_groups
+from util import create_groups, generate_test_user
 from entities.user import User, Rating
-import random
-import names
-import time
 import json
 
 def dump_to_json(obj):
@@ -11,29 +8,8 @@ def dump_to_json(obj):
 
 def simulate_matching():
     users = []
-    programs = ["ELEC", "COMP", "MECH", "ARCH", "CIVIL"]
-    current_time = int(time.time())
     for i in range(50):
-        users.append(User(
-            first_name=names.get_first_name(),
-            last_name=names.get_last_name(),
-            student_id=random.randint(100000, 999999),
-            program=random.choice(programs),
-            avatar_url="https://www.allaboutbirds.org/guide/assets/photo/59953191-480px.jpg",
-            bio="Life is bigcat",
-            ratings=Rating(
-                software=random.randint(0, 5),
-                leadership=random.randint(0, 5),
-                database=random.randint(0, 5),
-                writing=random.randint(0, 5),
-                hardware=random.randint(0, 5),
-                embedded=random.randint(0, 5)
-            ),
-            in_group=False,
-            group_id=0,
-            intent_stay=False,
-            join_date=current_time
-        ))
+        users.append(generate_test_user())
     
     groups = create_groups(users)
     print(groups)
--- backend/util.py
+++ backend/util.py
@@ -1,8 +1,9 @@
 from typing import List
 
-from entities.user import User
+from entities.user import User, Rating
 import random
-
+import names
+import time
 
 # Temporary impl. (completely random)
 def create_groups(users: List[User], size=4):
@@ -26,4 +27,27 @@ def create_groups(users: List[User], size=4):
             overflow[i].append(users[i])
     
     return groups
-        
+
+def generate_test_user():
+    programs = ["ELEC", "COMP", "MECH", "ARCH", "CIVIL"]
+    current_time = int(time.time())
+    return User(
+        first_name=names.get_first_name(),
+        last_name=names.get_last_name(),
+        student_id=random.randint(100000, 999999),
+        program=random.choice(programs),
+        avatar_url="https://www.allaboutbirds.org/guide/assets/photo/59953191-480px.jpg",
+        bio="Life is bigcat",
+        ratings=Rating(
+            software=random.randint(0, 5),
+            leadership=random.randint(0, 5),
+            database=random.randint(0, 5),
+            writing=random.randint(0, 5),
+            hardware=random.randint(0, 5),
+            embedded=random.randint(0, 5)
+        ),
+        in_group=False,
+        group_id=0,
+        intent_stay=False,
+        join_date=current_time
+    )
