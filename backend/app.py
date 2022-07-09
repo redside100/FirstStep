0d9e1cfa4baf0f8e4e339b516c8c65723053e995
s367pate <s367pate@uwaterloo.ca>
2022-07-09 16:01:55 -0400

    Finished basic API routes, will require actual logic when other components are set up

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from util import generate_test_user
+from flask import Flask, request, jsonify
+from util import generate_test_user, create_groups
 
 app = Flask(__name__)
 
@@ -12,51 +12,56 @@ def hello_world():  # put application's code here
 @app.post('/user')
 def create_user():
     #TODO create user and return new user id
-    return generate_test_user()
+    user = request.get_json()
+    return '', 204
 
 @app.get('/user/<int:user_id>/profile')
 def get_user_profile(user_id):
     #TODO: get user from schema
-    return user_id
+    return jsonify(generate_test_user()), 200
 
-@app.get('/user/<int:user_id>/profile')
+@app.post('/user/<int:user_id>/profile')
 def post_user_profile(user_id):
     #TODO: update user from request
-    return user_id
+    return "updated user", 204
 
 @app.get('/group/<int:group_id>')
 def get_group(group_id):
-    return id:
+    users = []
+    for i in range(4):
+        users.append(generate_test_user())
+    group = create_groups(users, 4)
+    return jsonify(group), 200
 
 @app.post('/group/<int:group_id>/disband')
 def disband_group(group_id):
     #Return status of group being deleted
     #TODO: disband group
-    return True
+    return '', 204
 
 @app.post('/group/<int:group_id>/commit')
 def commit_group(group_id):
     #Return status of group being created
     #TODO: commit group
-    return True
+    return '', 204
 
-@app.get('/matching')
+@app.get('/matching/status')
 def get_matching():
     user_id = request.args.get('userid')
-    return user_id
+    return {"matching": "true"}, 200
 
 @app.post('/matching')
 def post_matching():
     #TODO: update matching data from request
-    return 0
+    return '', 204
 
 @app.get('/skillsets/all')
 def get_all_skillsets():
-    return ""
+    return {"leadership" : "1", "communication": "3"}, 200
 
 @app.get('/preferences/all')
 def get_all_preferences():
-    return ""
+    return {"preferences": { "hardware": "true", "embedded systems": "true"}}, 200
 
 if __name__ == '__main__':
     app.run()
